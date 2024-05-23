7ba0973a49a03d5ca69938c17ad50ec718ca362f
beatrizveloso <beatrizoslov6@gmail.com>
2024-05-23 09:21:20 -0300

    feat: save aula 23.05

---
+++ aula  - 23.05.py
@@ -0,0 +1,38 @@
+# Insira os valores do salário de cada mês:
+mes = ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"]
+
+for i in range(12):
+  salario = float(input("Digite o salário de %s R$" % mes[i]))
+  
+  
+  
+# Faça um programa em Python que liste 3 nomes:
+nome = ['Harry', 'Rony', 'Hermione' ]
+for i in range(3):
+  print(nome[i])
+ 
+ 
+  
+# Faça um programa em Python que calcule a média de um aluno apartir de cinco notas :
+
+notas = [5, 6, 7, 8.7, 4.5]
+qtde = len(notas)
+
+
+
+# Iterando sobre a lista usando indices
+soma = 0
+for i in range(5):
+  soma = soma + notas[i]
+media = soma / 5
+print("A média é %f." % media)
+
+# Iterando sobre os elementos da lista
+soma = 0
+for nota in notas:
+  soma += nota
+media = soma / 5
+print("A média é %f." % media)
+
+
+
